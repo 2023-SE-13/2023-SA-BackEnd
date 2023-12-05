377b3bcb4daeb92424273085e380c2742ebb0065
nekovoo <1398993569@qq.com>
2023-12-05 20:15:38 +0800

    新增登陆注册

--- user/views.py
+++ user/views.py
@@ -1,3 +1,39 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from user.models import User
+
+
 # Create your views here.
+def register(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+
+    if User.objects.filter(username=username).exists():
+        result = {'result': 1, 'message': r'用户名已存在'}
+        return JsonResponse(result)
+
+    email = request.POST.get('email', '')
+    user = User.objects.create(username=username, password=password, email=email)
+    user.save()
+    result = {'result': 0, 'message': r'注册成功'}
+    return JsonResponse(result)
+
+def login(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if not User.objects.filter(username=username):
+        result = {'result': 1, 'message': r'用户名或密码错误'}
+        return JsonResponse(result)
+    user = User.objects.get(username=username)
+    if user.password == password:
+        request.session['username'] = username
+        user = User.objects.get(username=username)
+        user.is_login = True
+        user.save()
+        result = {'result': 0, 'message': r'登录成功'}
+        return JsonResponse(result)
+    else:
+        result = {'result': 1, 'message': r'用户名或密码错误'}
+        return JsonResponse(result)
+
