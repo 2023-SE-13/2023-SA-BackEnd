c46eea7e0d9bbde84053afb50d57f34258bf5211
Leonard <leonardzhou2077@gmail.com>
2023-12-19 21:36:24 +0800

    update

--- SA_backend/settings.py
+++ SA_backend/settings.py
@@ -174,3 +174,9 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_USE_TLS = True  # 是否使用TLS安全传输协议
 
 AUTH_USER_MODEL = 'user.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
