224271d830eff0adc0f8b85e0a805bf3cbc9a9a5
Hansuero <93417959+Hansuero@users.noreply.github.com>
2023-12-21 21:20:30 +0800

    扩大标题容量

--- Academia/models.py
+++ Academia/models.py
@@ -47,7 +47,7 @@ class Patent(models.Model):
 class Work_Data(models.Model):
     browse_times = models.IntegerField(default=0)
     work_id = models.CharField('work_id',max_length=40)
-    title = models.CharField('title',max_length=40)
+    title = models.CharField('title',max_length=100)
 
 
 class Work_Author(models.Model):
@@ -57,5 +57,5 @@ class Work_Author(models.Model):
 
 class Favorite(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    article_name = models.CharField( max_length=40)
+    article_name = models.CharField( max_length=100)
     article_id = models.CharField(max_length=40)
