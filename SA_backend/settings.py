197da516df17366ffc6068cdef6024e26f92576f
Leonard <leonardzhou2077@gmail.com>
2023-12-21 10:38:28 +0800

    update settings.py

--- SA_backend/settings.py
+++ SA_backend/settings.py
@@ -93,21 +93,14 @@ WSGI_APPLICATION = 'SA_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DATABASE_ENGINE'),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv('DATABASE_NAME'),
-#         'USER': os.getenv('DATABASE_USER'),
-#         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-#         'HOST': '116.63.49.180',
-#         'PORT': '3306',
-#     }
-#
-# }
 
 
 # Password validation
