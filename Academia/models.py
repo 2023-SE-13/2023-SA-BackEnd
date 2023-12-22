0d2d736ec13e4e9a529e96f61108f5bcde2b2591
Hansuero <93417959+Hansuero@users.noreply.github.com>
2023-12-22 15:24:25 +0800

    移除无用的模型

--- Academia/models.py
+++ Academia/models.py
@@ -3,47 +3,6 @@ from django.db import models
 from user.models import User
 
 
-class Paper(models.Model):
-    title = models.TextField()
-    keywords = models.TextField()
-    citation_count = models.IntegerField()
-    page_start = models.IntegerField()
-    page_end = models.IntegerField()
-    type = models.CharField(max_length=20)
-    language = models.CharField(max_length=20)
-    publisher = models.CharField(max_length=20)
-    volume = models.CharField(max_length=20)
-    issue = models.CharField(max_length=20)
-    issn = models.CharField(max_length=20)
-    isbn = models.CharField(max_length=20)
-    doi = models.CharField(max_length=20)
-    pdf_link = models.CharField(max_length=50)
-    url = models.CharField(max_length=50)
-    abstract = models.TextField()
-    last_update_time = models.DateTimeField(null=True)
-    venue_id = models.IntegerField()
-
-
-
-class Patent(models.Model):
-    id = models.CharField(max_length=40, primary_key=True)
-    title = models.TextField()
-    keywords = models.TextField()
-    citation_count = models.IntegerField()
-    page_start = models.IntegerField()
-    page_end = models.IntegerField()
-    type = models.CharField(max_length=20)
-    language = models.CharField(max_length=20)
-    publisher = models.CharField(max_length=20)
-    volume = models.CharField(max_length=20)
-    issue = models.CharField(max_length=20)
-    pdf_link = models.CharField(max_length=50)
-    url = models.CharField(max_length=50)
-    abstract = models.TextField()
-    last_update_time = models.DateTimeField()
-    venue_id = models.IntegerField()
-
-
 class Work_Data(models.Model):
     browse_times = models.IntegerField(default=0)
     work_id = models.CharField('work_id',max_length=40)
