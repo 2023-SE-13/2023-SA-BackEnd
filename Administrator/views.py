5974bac70a3629ce9493a17ac0c9fa01f6c12691
nekovoo <1398993569@qq.com>
2023-12-26 00:33:06 +0800

    增加datetime

--- Administrator/views.py
+++ Administrator/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import authentication_classes, permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
-
+from django.utils.dateformat import DateFormat
 from Academia.models import Work_Author
 from message.models import ApplyBeAuthor, ApplyWork, ReplyToUser
 from user.models import User, Author_User
@@ -24,8 +24,9 @@ def show_author_message(request):
             messages_list = [{
                 'id': message.id,
                 'author_id': message.author_id,
-                'send_user': message.send_user.username,  # 假设你想返回发送用户的用户名
+                'username': message.send_user.username,  # 假设你想返回发送用户的用户名
                 'send_user_id': message.send_user.id,
+                'datetime': DateFormat(message.created_at).format('Y-m-d H:i')
             } for message in messages]
             result = {'result': 0, 'messages': messages_list}
             return JsonResponse(result)
--- message/models.py
+++ message/models.py
@@ -14,11 +14,15 @@ class ApplyBeAuthor(models.Model):
     photo = models.CharField('消息图片路径', max_length=128, default='', null=True)
     photo_out = models.CharField('外部消息图片路径', max_length=128, default='', null=True)
     author_id = models.CharField('学者id',max_length=40)
+    created_at = models.DateTimeField('创建时间', auto_now_add=True,null=True)
+
 
 
 class ApplyWork(models.Model):
-    work_id = models.CharField('成果id',max_length=40)
+    work_id = models.CharField('成果id', max_length=40)
     send_user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='发送用户', default='')
+    created_at = models.DateTimeField('创建时间', auto_now_add=True,null=True)
+
 
 
 class ReplyToUser(models.Model):
