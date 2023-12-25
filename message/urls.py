575dcff125131d80047571e24b9cc6d4b4de98b4
nekovoo <1398993569@qq.com>
2023-12-26 04:44:02 +0800

    show_all

--- message/urls.py
+++ message/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from message.views import show_hot
+from message.views import *
 
 urlpatterns = [
     # path('url_name', api_name)
     # 这是一个样例，指定路由名为url_name，对应处理函数为当前app内views.py中的api_name
     path('show_hot', show_hot),
+    path('show_all', show_all),
+
 ]
